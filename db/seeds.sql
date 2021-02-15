947fd8420978cfe6087abca3f187e151e36f9513
Veronika Balakhovsky <vbalakhovsky32@gmail.com>
2021-02-15 15:44:55 -0500

    changed the whole app

--- db/seeds.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO burgers (name) VALUES ('Vegan Delight');
-INSERT INTO burgers (name) VALUES ('Grilled Portobello');
-INSERT INTO burgers (name, devoured) VALUES ('Greek', true);
-INSERT INTO burgers (name, devoured) VALUES ('Grilled Peppers', true);
-INSERT INTO burgers (name, devoured) VALUES ('Extra spicy', true);
-INSERT INTO burgers (name) VALUES ('Kobe beef');
